490a5003e35cff7475d0cbe23770904eb720570e
Nathan Cartwright <fshost@yahoo.com>
2013-06-11 22:00:35 -0500

    add tests for is{Type} methods

--- test/test.js
+++ test/test.js
@@ -4,18 +4,18 @@ var path = require('path'),
     test = require('tap').test,
     jsType = require('..');
 
-test("jsType module", function (test) {
+test("jsType module", function(test) {
 
-    test.test("type", function (test) {
+    test.test("type", function(test) {
         var type = jsType.type;
         test.ok(type);
-        test.test("base types", function (test) {
+        test.test("base types", function(test) {
             test.equal(type(undefined), 'undefined');
             test.equal(type(null), 'null');
             test.equal(type(NaN), 'number');
             test.equal(type('test string'), 'string');
             test.equal(type(new Date('1-1-2011')), 'date');
-            test.equal(type(function test() { }), 'function');
+            test.equal(type(function test() {}), 'function');
             test.equal(type(true), 'boolean');
             test.equal(type(false), 'boolean');
             test.equal(type(-1), 'number');
@@ -26,11 +26,13 @@ test("jsType module", function (test) {
             test.equal(type(['a', 'b', 'c']), 'array');
             test.equal(type(/[a-z]/), 'regexp');
             test.equal(type({}), 'object');
-            test.equal(type({ foo: 'bar' }), 'object');
+            test.equal(type({
+                foo: 'bar'
+            }), 'object');
             test.end();
         });
 
-        test.test("extended numeric types", function (test) {
+        test.test("extended numeric types", function(test) {
             test.equal(type(1, true), 'integer');
             test.equal(type(1.1, true), 'float');
             test.equal(type(1 / 0, true), 'infinity');
@@ -39,11 +41,13 @@ test("jsType module", function (test) {
             test.end();
         });
 
-        test.test("custom defined types", function (test) {
+        test.test("custom defined types", function(test) {
             test.ok(jsType.define);
             // test custom defined types
-            function Custom1() { };
-            function Custom2() { };
+
+            function Custom1() {}
+
+            function Custom2() {}
             jsType.define(Custom1);
             jsType.define('custom2', Custom2);
 
@@ -52,7 +56,7 @@ test("jsType module", function (test) {
             test.equal(type(instance1, true), 'Custom1');
             test.equal(type(instance2, true), 'custom2');
 
-            test.test("baseNumeric argument with extended types", function (test) {
+            test.test("baseNumeric argument with extended types", function(test) {
                 test.equal(type(1, true, true), 'number');
                 test.equal(type(instance1, true, true), 'Custom1');
                 test.equal(type(instance2, false, true), 'object');
@@ -63,6 +67,19 @@ test("jsType module", function (test) {
 
         });
 
+        test.test("built-in isType methods", function(test) {
+            test.ok(jsType.isNumber(1));
+            test.ok(jsType.isString('foo'));
+            test.ok(jsType.isObject({
+                foo: 'bar'
+            }));
+            test.ok(jsType.isArray([1, 2]));
+            test.ok(jsType.isDate(new Date()));
+            test.ok(jsType.isRegexp(/./g));
+            test.ok(jsType.isFunction(function () {}));
+            test.end();
+        });
+
         test.end();
 
     });
@@ -70,4 +87,3 @@ test("jsType module", function (test) {
     test.end();
 
 });
-
